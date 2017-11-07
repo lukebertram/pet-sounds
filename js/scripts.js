78075fefc8e17b61fe04bc6ae4870a4af90557bd
Luke Bertram and Lena Ha <lena@>
2017-11-06 16:37:51 -0800

    place pet names atop pet pictures

--- js/scripts.js
+++ js/scripts.js
@@ -9,15 +9,26 @@ function Pet (name, age, gender, type, image) {
 
 
 var pets = [];
-pets.push(new Pet("Spots", "15000", "Lichcat", "http://placekitten.com/g/200/300"));
-pets.push(new Pet("Tinkles", "2", "Mutahound", "http://placekitten.com/g/200/300"));
-pets.push(new Pet("Sgt. Tumbles", "37", "Human", "http://placekitten.com/g/200/300"));
-pets.push(new Pet("Dyna", "7", "Kitty", "http://placekitten.com/g/200/300"));
-pets.push(new Pet("Alice", "12", "Radbit", "http://placekitten.com/g/200/300"));
+pets.push(new Pet("Spots", "15000", "male", "Lichcat", "http://fillmurray.com/200/300"));
+pets.push(new Pet("Tinkles", "2", "female", "Mutahound", "http://fillmurray.com/200/300"));
+pets.push(new Pet("Sgt. Tumbles", "37", "attack helicopter", "Human", "http://fillmurray.com/200/300"));
+pets.push(new Pet("Dyna", "7", "female", "Kitty", "http://fillmurray.com/200/300"));
+pets.push(new Pet("Alice", "12", "female", "Radbit", "http://fillmurray.com/200/300"));
 
 
 //front-end flamboyance
 $(document).ready(function(){
+
+  //display all pets in the array
+  pets.forEach (function(pet){
+    $(".pet-list ul").append(
+       "<li>" +
+         "<div class='pet-preview'>" +
+          "<img src='" + pet.image + "' alt='" + pet.name + "'>" +
+          "<span class='pet-name'>" + pet.name + "</span>" +
+        "</div>" +
+       "</li>");
+  });
 
   //on form submit
   $("form#pet-form").submit(function(event){
